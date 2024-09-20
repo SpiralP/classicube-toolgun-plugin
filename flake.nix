e41a6f8e13bfdfaed41fcda26e0488e4464c2694
SpiralP <spiral.palmer@gmail.com>
2024-09-20 14:54:16 -0700

    nix!

---
+++ flake.nix
@@ -0,0 +1,86 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      inherit (nixpkgs) lib;
+
+      makePackages = (system: dev:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+          rustManifest = lib.importTOML ./Cargo.toml;
+
+          defaultAttrs = {
+            pname = rustManifest.package.name;
+            version = rustManifest.package.version;
+
+            src = lib.sourceByRegex ./. [
+              "^\.cargo(/.*)?$"
+              "^build\.rs$"
+              "^Cargo\.(lock|toml)$"
+              "^sounds(/.*)?$"
+              "^src(/.*)?$"
+            ];
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              outputHashes = {
+                "async-dispatcher-0.1.0" = "sha256-rqpQ176/PnI9vvPrwQvK3GJbryjb3hHkb+o1RyCZ3Vg=";
+                "classicube-helpers-2.0.0+classicube.1.3.6" = "sha256-NgpBwlYCJyBV+oKyVyU5ueI9AubEZ0tsrsH+eQFF3Xk=";
+              };
+            };
+
+            buildInputs = with pkgs; [
+              alsa-lib
+              at-spi2-atk
+              cairo
+              gdk-pixbuf
+              glib
+              gtk3
+              openssl
+              pango
+            ];
+
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              rustPlatform.bindgenHook
+            ] ++ (if dev then
+              with pkgs; ([
+                cargo-release
+                clippy
+                (rustfmt.override { asNightly = true; })
+                rust-analyzer
+              ]) else [ ]);
+          };
+        in
+        {
+          default = pkgs.rustPlatform.buildRustPackage defaultAttrs;
+
+          debug = (pkgs.enableDebugging {
+            inherit (pkgs) stdenv;
+            override = (attrs: pkgs.makeRustPlatform ({
+              inherit (pkgs) rustc cargo;
+            } // attrs));
+          }).buildRustPackage (
+            (defaultAttrs // {
+              pname = "${defaultAttrs.pname}-debug";
+
+              buildType = "debug";
+
+              hardeningDisable = [ "all" ];
+            })
+          );
+        }
+      );
+    in
+    builtins.foldl' lib.recursiveUpdate { } (builtins.map
+      (system: {
+        devShells.${system} = makePackages system true;
+        packages.${system} = makePackages system false;
+      })
+      lib.systems.flakeExposed);
+}
